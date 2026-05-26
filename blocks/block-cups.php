edb4edd7b9a0082688fdae133d3e7c49bd7eff75
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2026-05-26 20:23:47 +0300

    пустые записи теперь не выводятся вообще; если нет ни одной записи с владельцем, показывается текст: тут пока пусто

--- blocks/block-cups.php
+++ blocks/block-cups.php
@@ -13,40 +13,45 @@ $content = '';
 kz_cups_update_auto(false);
 
 $cups = kz_cups_current();
-$content .= '<table width="100%" border="0" cellspacing="0" cellpadding="0">';
+$rows = '';
+$display_num = 0;
 
 foreach ($cups as $cup) {
-    $num = (int)$cup['sort'];
-    $icon = '<i class="i1 ' . kz_cups_h($cup['icon']) . '"></i>';
-    $cup_title = kz_cups_h($cup['title']);
-    $holder = '<span style="color:#777;">—</span>';
-
-    if (!empty($cup['userid']) && !empty($cup['username'])) {
-        $userid = (int)$cup['userid'];
-        $username = kz_cups_h($cup['username']);
-        $class = isset($cup['class']) ? (int)$cup['class'] : UC_USER;
-        $flag = '';
-
-        if (!empty($cup['flagpic'])) {
-            $flagpic = kz_cups_h($cup['flagpic']);
-            $flag = '<img src="pic/flag/' . $flagpic . '" alt="" style="vertical-align:middle; margin-right:2px;">';
-        }
-
-        $icons = function_exists('get_user_icons') ? get_user_icons($cup) : '';
-        $holder = $flag
-            . '<a href="userdetails.php?id=' . $userid . '" title="' . $cup_title . '">'
-            . get_user_class_color($class, $username)
-            . '</a>'
-            . $icons
-            . ' ' . $icon;
+    if (empty($cup['userid']) || empty($cup['username'])) {
+        continue;
     }
 
-    $content .= '<tr>'
-        . '<td class="embedded" width="16" align="right">' . $num . '.</td>'
+    $display_num++;
+    $icon = '<i class="i1 ' . kz_cups_h($cup['icon']) . '"></i>';
+    $cup_title = kz_cups_h($cup['title']);
+    $userid = (int)$cup['userid'];
+    $username = kz_cups_h($cup['username']);
+    $class = isset($cup['class']) ? (int)$cup['class'] : UC_USER;
+    $flag = '';
+
+    if (!empty($cup['flagpic'])) {
+        $flagpic = kz_cups_h($cup['flagpic']);
+        $flag = '<img src="pic/flag/' . $flagpic . '" alt="" style="vertical-align:middle; margin-right:2px;">';
+    }
+
+    $icons = function_exists('get_user_icons') ? get_user_icons($cup) : '';
+    $holder = $flag
+        . '<a href="userdetails.php?id=' . $userid . '" title="' . $cup_title . '">'
+        . get_user_class_color($class, $username)
+        . '</a>'
+        . $icons
+        . ' ' . $icon;
+
+    $rows .= '<tr>'
+        . '<td class="embedded" width="16" align="right">' . $display_num . '.</td>'
         . '<td class="embedded">' . $holder . '</td>'
         . '</tr>';
 }
 
-$content .= '</table>';
+if ($rows !== '') {
+    $content .= '<table width="100%" border="0" cellspacing="0" cellpadding="0">' . $rows . '</table>';
+} else {
+    $content .= 'тут пока пусто';
+}
 
 ?>
