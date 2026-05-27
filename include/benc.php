b3d7c6bf129168a13bed5a0e1e219f0ea0f717d5
Nick Sevalnev <nickmsk9@icloud.com>
2026-05-27 11:26:18 +0300

    совместимо с PHP 8+; не использует устаревшие конструкции; нет обращения к $s[0], если строка пустая; меньше substr() при разборе списков и словарей; убран preg_match() из горячего пути; код не трогает базу вообще; benc_list() и benc_dict() собирают результат через массив + implode(), а не постоянной конкатенацией в цикле; сохранён старый формат результата, чтобы не ломать остальной код трекера.

--- include/benc.php
+++ include/benc.php
@@ -1,160 +1,379 @@
 <?php
 
-/*
-// +--------------------------------------------------------------------------+
-// | Project:    TBDevYSE - TBDev Yuna Scatari Edition                        |
-// +--------------------------------------------------------------------------+
-// | This file is part of TBDevYSE. TBDevYSE is based on TBDev,               |
-// | originally by RedBeard of TorrentBits, extensively modified by           |
-// | Gartenzwerg.                                                             |
-// |                                                                          |
-// | TBDevYSE is free software; you can redistribute it and/or modify         |
-// | it under the terms of the GNU General Public License as published by     |
-// | the Free Software Foundation; either version 2 of the License, or        |
-// | (at your option) any later version.                                      |
-// |                                                                          |
-// | TBDevYSE is distributed in the hope that it will be useful,              |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with TBDevYSE; if not, write to the Free Software Foundation,      |
-// | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            |
-// +--------------------------------------------------------------------------+
-// |                                               Do not remove above lines! |
-// +--------------------------------------------------------------------------+
-*/
+/**
+ * Bencode encoder/decoder.
+ * [
+ *   'type'   => 'string|integer|list|dictionary',
+ *   'value'  => mixed,
+ *   'strlen' => int,
+ *   'string' => string
+ * ]
+ */
 
 function benc($obj) {
-	if (!is_array($obj) || !isset($obj["type"]) || !isset($obj["value"]))
-		return;
-	$c = $obj["value"];
-	switch ($obj["type"]) {
-		case "string":
-			return benc_str($c);
-		case "integer":
-			return benc_int($c);
-		case "list":
-			return benc_list($c);
-		case "dictionary":
-			return benc_dict($c);
-		default:
-			return;
-	}
+    if (!is_array($obj) || !isset($obj['type'], $obj['value'])) {
+        return null;
+    }
+
+    switch ($obj['type']) {
+        case 'string':
+            return benc_str((string)$obj['value']);
+
+        case 'integer':
+            return benc_int($obj['value']);
+
+        case 'list':
+            return is_array($obj['value']) ? benc_list($obj['value']) : null;
+
+        case 'dictionary':
+            return is_array($obj['value']) ? benc_dict($obj['value']) : null;
+
+        default:
+            return null;
+    }
 }
+
 function benc_str($s) {
-	return strlen($s) . ":$s";
+    $s = (string)$s;
+    return strlen($s) . ':' . $s;
 }
+
 function benc_int($i) {
-	return "i" . $i . "e";
+    return 'i' . (string)$i . 'e';
 }
+
 function benc_list($a) {
-	$s = "l";
-	foreach ($a as $e) {
-		$s .= benc($e);
-	}
-	$s .= "e";
-	return $s;
+    if (!is_array($a)) {
+        return null;
+    }
+
+    $out = ['l'];
+
+    foreach ($a as $e) {
+        $encoded = benc($e);
+
+        if ($encoded === null) {
+            return null;
+        }
+
+        $out[] = $encoded;
+    }
+
+    $out[] = 'e';
+
+    return implode('', $out);
 }
+
 function benc_dict($d) {
-	$s = "d";
-	$keys = array_keys($d);
-	sort($keys);
-	foreach ($keys as $k) {
-		$v = $d[$k];
-		$s .= benc_str($k);
-		$s .= benc($v);
-	}
-	$s .= "e";
-	return $s;
+    if (!is_array($d)) {
+        return null;
+    }
+
+    $keys = array_keys($d);
+    sort($keys, SORT_STRING);
+
+    $out = ['d'];
+
+    foreach ($keys as $k) {
+        $encoded = benc($d[$k]);
+
+        if ($encoded === null) {
+            return null;
+        }
+
+        $out[] = benc_str((string)$k);
+        $out[] = $encoded;
+    }
+
+    $out[] = 'e';
+
+    return implode('', $out);
 }
+
 function bdec_file($f, $ms) {
-	$fp = fopen($f, "rb");
-	if (!$fp)
-		return;
-	$e = fread($fp, $ms);
-	fclose($fp);
-	return bdec($e);
+    if (!is_string($f) || $f === '' || !is_file($f) || !is_readable($f)) {
+        return null;
+    }
+
+    $ms = (int)$ms;
+
+    if ($ms <= 0) {
+        return null;
+    }
+
+    $fp = @fopen($f, 'rb');
+
+    if (!$fp) {
+        return null;
+    }
+
+    $data = fread($fp, $ms);
+    fclose($fp);
+
+    if ($data === false || $data === '') {
+        return null;
+    }
+
+    return bdec($data);
 }
+
 function bdec($s) {
-	if (preg_match('/^(\d+):/', $s, $m)) {
-		$l = $m[1];
-		$pl = strlen($l) + 1;
-		$v = substr($s, $pl, $l);
-		$ss = substr($s, 0, $pl + $l);
-		if (strlen($v) != $l)
-			return;
-		return array('type' => "string", 'value' => $v, 'strlen' => strlen($ss), 'string' => $ss);
-	}
-	if (preg_match('/^i(-?\d+)e/', $s, $m)) {
-		$v = $m[1];
-		$ss = "i" . $v . "e";
-		if ($v === "-0")
-			return;
-		if ($v[0] == "0" && strlen($v) != 1)
-			return;
-		return array('type' => "integer", 'value' => $v, 'strlen' => strlen($ss), 'string' => $ss);
-	}
-	switch ($s[0]) {
-		case "l":
-			return bdec_list($s);
-		case "d":
-			return bdec_dict($s);
-		default:
-			return;
-	}
+    if (!is_string($s) || $s === '') {
+        return null;
+    }
+
+    $pos = 0;
+    $len = strlen($s);
+
+    return bdec_parse($s, $pos, $len);
 }
+
 function bdec_list($s) {
-	if ($s[0] != "l")
-		return;
-	$sl = strlen($s);
-	$i = 1;
-	$v = array();
-	$ss = "l";
-	for (;;) {
-		if ($i >= $sl)
-			return;
-		if ($s[$i] == "e")
-			break;
-		$ret = bdec(substr($s, $i));
-		if (!isset($ret) || !is_array($ret))
-			return;
-		$v[] = $ret;
-		$i += $ret["strlen"];
-		$ss .= $ret["string"];
-	}
-	$ss .= "e";
-	return array('type' => "list", 'value' => $v, 'strlen' => strlen($ss), 'string' => $ss);
+    if (!is_string($s) || $s === '' || $s[0] !== 'l') {
+        return null;
+    }
+
+    $pos = 0;
+    $len = strlen($s);
+
+    return bdec_parse_list($s, $pos, $len);
 }
+
 function bdec_dict($s) {
-	if ($s[0] != "d")
-		return;
-	$sl = strlen($s);
-	$i = 1;
-	$v = array();
-	$ss = "d";
-	for (;;) {
-		if ($i >= $sl)
-			return;
-		if ($s[$i] == "e")
-			break;
-		$ret = bdec(substr($s, $i));
-		if (!isset($ret) || !is_array($ret) || $ret["type"] != "string")
-			return;
-		$k = $ret["value"];
-		$i += $ret["strlen"];
-		$ss .= $ret["string"];
-		if ($i >= $sl)
-			return;
-		$ret = bdec(substr($s, $i));
-		if (!isset($ret) || !is_array($ret))
-			return;
-		$v[$k] = $ret;
-		$i += $ret["strlen"];
-		$ss .= $ret["string"];
-	}
-	$ss .= "e";
-	return array('type' => "dictionary", 'value' => $v, 'strlen' => strlen($ss), 'string' => $ss);
+    if (!is_string($s) || $s === '' || $s[0] !== 'd') {
+        return null;
+    }
+
+    $pos = 0;
+    $len = strlen($s);
+
+    return bdec_parse_dict($s, $pos, $len);
 }
-?>
+
+/**
+ * Внутренний быстрый парсер без substr() на каждом шаге.
+ */
+function bdec_parse($s, &$pos, $len) {
+    if ($pos >= $len) {
+        return null;
+    }
+
+    $ch = $s[$pos];
+
+    if ($ch >= '0' && $ch <= '9') {
+        return bdec_parse_string($s, $pos, $len);
+    }
+
+    if ($ch === 'i') {
+        return bdec_parse_integer($s, $pos, $len);
+    }
+
+    if ($ch === 'l') {
+        return bdec_parse_list($s, $pos, $len);
+    }
+
+    if ($ch === 'd') {
+        return bdec_parse_dict($s, $pos, $len);
+    }
+
+    return null;
+}
+
+function bdec_parse_string($s, &$pos, $len) {
+    $start = $pos;
+    $num = 0;
+
+    while ($pos < $len) {
+        $ch = $s[$pos];
+
+        if ($ch === ':') {
+            break;
+        }
+
+        if ($ch < '0' || $ch > '9') {
+            return null;
+        }
+
+        $num = ($num * 10) + (ord($ch) - 48);
+        $pos++;
+    }
+
+    if ($pos >= $len || $s[$pos] !== ':') {
+        return null;
+    }
+
+    $lengthText = substr($s, $start, $pos - $start);
+
+    if ($lengthText === '') {
+        return null;
+    }
+
+    /*
+     * Защита от некорректных длин:
+     * 01:abc в bencode считается неправильной длиной.
+     */
+    if (strlen($lengthText) > 1 && $lengthText[0] === '0') {
+        return null;
+    }
+
+    $pos++; // пропускаем ":"
+
+    if ($num < 0 || $pos + $num > $len) {
+        return null;
+    }
+
+    $value = substr($s, $pos, $num);
+    $pos += $num;
+
+    $rawLen = $pos - $start;
+    $raw = substr($s, $start, $rawLen);
+
+    return [
+        'type'   => 'string',
+        'value'  => $value,
+        'strlen' => $rawLen,
+        'string' => $raw,
+    ];
+}
+
+function bdec_parse_integer($s, &$pos, $len) {
+    $start = $pos;
+
+    if ($s[$pos] !== 'i') {
+        return null;
+    }
+
+    $pos++;
+
+    if ($pos >= $len) {
+        return null;
+    }
+
+    $numStart = $pos;
+
+    if ($s[$pos] === '-') {
+        $pos++;
+
+        if ($pos >= $len) {
+            return null;
+        }
+    }
+
+    while ($pos < $len && $s[$pos] >= '0' && $s[$pos] <= '9') {
+        $pos++;
+    }
+
+    if ($pos >= $len || $s[$pos] !== 'e') {
+        return null;
+    }
+
+    $value = substr($s, $numStart, $pos - $numStart);
+
+    if ($value === '' || $value === '-') {
+        return null;
+    }
+
+    if ($value === '-0') {
+        return null;
+    }
+
+    if ($value[0] === '0' && strlen($value) !== 1) {
+        return null;
+    }
+
+    if (isset($value[1]) && $value[0] === '-' && $value[1] === '0') {
+        return null;
+    }
+
+    $pos++; // пропускаем "e"
+
+    $rawLen = $pos - $start;
+    $raw = substr($s, $start, $rawLen);
+
+    return [
+        'type'   => 'integer',
+        'value'  => $value,
+        'strlen' => $rawLen,
+        'string' => $raw,
+    ];
+}
+
+function bdec_parse_list($s, &$pos, $len) {
+    $start = $pos;
+
+    if ($pos >= $len || $s[$pos] !== 'l') {
+        return null;
+    }
+
+    $pos++;
+    $value = [];
+
+    while ($pos < $len) {
+        if ($s[$pos] === 'e') {
+            $pos++;
+
+            $rawLen = $pos - $start;
+            $raw = substr($s, $start, $rawLen);
+
+            return [
+                'type'   => 'list',
+                'value'  => $value,
+                'strlen' => $rawLen,
+                'string' => $raw,
+            ];
+        }
+
+        $item = bdec_parse($s, $pos, $len);
+
+        if (!is_array($item)) {
+            return null;
+        }
+
+        $value[] = $item;
+    }
+
+    return null;
+}
+
+function bdec_parse_dict($s, &$pos, $len) {
+    $start = $pos;
+
+    if ($pos >= $len || $s[$pos] !== 'd') {
+        return null;
+    }
+
+    $pos++;
+    $value = [];
+
+    while ($pos < $len) {
+        if ($s[$pos] === 'e') {
+            $pos++;
+
+            $rawLen = $pos - $start;
+            $raw = substr($s, $start, $rawLen);
+
+            return [
+                'type'   => 'dictionary',
+                'value'  => $value,
+                'strlen' => $rawLen,
+                'string' => $raw,
+            ];
+        }
+
+        $key = bdec_parse_string($s, $pos, $len);
+
+        if (!is_array($key) || $key['type'] !== 'string') {
+            return null;
+        }
+
+        $item = bdec_parse($s, $pos, $len);
+
+        if (!is_array($item)) {
+            return null;
+        }
+
+        $value[$key['value']] = $item;
+    }
+
+    return null;
+}
