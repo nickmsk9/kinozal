a89537c4832e96fbf9b746c932b97664b0b343e2
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2026-05-30 06:38:55 +0300

    fix

--- groupexlist.php
+++ groupexlist.php
@@ -15,29 +15,29 @@ $sort = (int)($_GET['sort'] ?? 0);
 
 $where = array("visible = 'yes'");
 if ($name !== '') {
-	$where[] = 'name LIKE ' . sqlesc('%' . $name . '%', true);
+    $where[] = 'name LIKE ' . sqlesc('%' . $name . '%', true);
 }
 if ($userid > 0) {
-	$where[] = 'owner_id = ' . $userid;
+    $where[] = 'owner_id = ' . $userid;
 }
 if (isset(kz_groups_types()[$type])) {
-	$where[] = 'type = ' . $type;
+    $where[] = 'type = ' . $type;
 }
 if (isset(kz_groups_categories()[$cat])) {
-	$where[] = 'cat = ' . $cat;
+    $where[] = 'cat = ' . $cat;
 }
 if ($subcat > 0) {
-	$where[] = 'subcat = ' . $subcat;
+    $where[] = 'subcat = ' . $subcat;
 }
 
 $where_sql = 'WHERE ' . implode(' AND ', $where);
 $order_sql = 'created_at DESC, id DESC';
 if ($sort === 1) {
-	$order_sql = 'members_count DESC, created_at DESC';
+    $order_sql = 'members_count DESC, created_at DESC';
 } elseif ($sort === 2) {
-	$order_sql = 'torrents_count DESC, created_at DESC';
+    $order_sql = 'torrents_count DESC, created_at DESC';
 } elseif ($sort === 3) {
-	$order_sql = 'zabor_count DESC, created_at DESC';
+    $order_sql = 'zabor_count DESC, created_at DESC';
 }
 
 $res = sql_query("SELECT COUNT(*) FROM groupex_groups $where_sql") or sqlerr(__FILE__, __LINE__);
@@ -48,7 +48,7 @@ $params = $_GET;
 unset($params['page']);
 $href = '/groupexlist.php?';
 if ($params) {
-	$href .= http_build_query($params, '', '&amp;') . '&amp;';
+    $href .= http_build_query($params, '', '&amp;') . '&amp;';
 }
 
 $perpage = 20;
@@ -68,43 +68,51 @@ kz_groups_subcat_script(array('gsearch_subcatsel' => $subcat));
 
 ?>
 <div class="bx2">
-	<div class="pad0x0x5x0">
-		<a href="/groupexlist.php" class="sbab">Список групп</a>
-		::
-		<a href="/mygroups.php" class="sbab">Мои группы</a>
-	</div>
-	<?php kz_groups_search_sidebar(); ?>
-	<div class="mn3_content">
-		<div class="bx1">
-			<span class="bulet"></span>
-			<b>Каталог групп</b>
-			<span class="floatright">Найдено групп: <b><?= (int)$count ?></b></span>
-			<div class="clr"></div>
-		</div>
-		<?php if ($pagertop) { ?>
-			<div class="pad0x0x5x0"><?= $pagertop ?></div>
-		<?php } ?>
-		<div class="bx2_0">
-			<?php if ($count < 1) { ?>
-				<div class="pad10x10 center">По заданным параметрам группы не найдены.</div>
-			<?php } else { ?>
-				<?php while ($group = mysqli_fetch_assoc($groups_res)) { ?>
-					<?php kz_groups_group_card($group); ?>
-				<?php } ?>
-			<?php } ?>
-		</div>
-		<?php if ($pagerbottom) { ?>
-			<div class="pad5x5"><?= $pagerbottom ?></div>
-		<?php } ?>
-	</div>
-	<div class="clr"></div>
+    <div class="pad0x0x5x0">
+        <a href="/groupexlist.php" class="sbab">Список групп</a>
+        ::
+        <a href="/mygroups.php" class="sbab">Мои группы</a>
+    </div>
+    <?php kz_groups_search_sidebar(); ?>
+    <div class="mn3_content">
+        <div class="bx1">
+            <span class="bulet"></span>
+            <b>Каталог групп</b>
+            <span class="floatright">Найдено групп: <b><?= (int)$count ?></b></span>
+            <div class="clr"></div>
+        </div>
+        <?php if ($pagertop) { ?>
+            <div class="pad0x0x5x0"><?= $pagertop ?></div>
+        <?php } ?>
+        <div class="bx2_0">
+            <?php if ($count < 1) { ?>
+                <div class="pad10x10 center">По заданным параметрам группы не найдены.</div>
+            <?php } else { ?>
+                <?php while ($group = mysqli_fetch_assoc($groups_res)) { ?>
+                    <?php kz_groups_group_card($group); ?>
+                <?php } ?>
+            <?php } ?>
+        </div>
+        <?php if ($pagerbottom) { ?>
+            <div class="pad5x5"><?= $pagerbottom ?></div>
+        <?php } ?>
+    </div>
+    <div class="clr"></div>
 </div>
 
 <div class="bx2_0">
-	<ul class="men">
-		<li class="tp2 center">Кто ОнЛайн здесь, на этой странице [ <a class="sba" href="/pay.php">помочь проекту</a> ]</li>
-		<li><div class="pad5x5"><?= kz_page_online_box(array('/groupexlist.php%'), 'никого нет на странице') ?></div></li>
-	</ul>
+    <ul class="men">
+        <li class="tp2 center">
+            Кто ОнЛайн здесь, на этой странице
+            [ <a class="sba" href="/pay.php">помочь проекту</a> ]
+        </li>
+
+        <li>
+            <div class="pad5x5">
+                <?= kz_page_online_box(array('/groupexlist.php%'), 'никого нет на странице'); ?>
+            </div>
+        </li>
+    </ul>
 </div>
 <?php
 stdfoot();
