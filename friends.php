7b137949a9a94606f4b0e636ba10ada971d1e3c2
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2026-05-31 18:54:16 +0300

    убрал warning на пустые id и action; меню пользователя теперь как в профиле; друзья выводятся карточками в 2 колонки с аватаром, ЛС, удалением, последним визитом, числом раздач и комментариев; игнорированные пользователи выводятся отдельным блоком в том же стиле; add/delete для друзей и игнора работают, при добавлении в друзья пользователь убирается из игнора и наоборот; заменил старый mysql_affected_rows() и некавыченные индексы.

--- friends.php
+++ friends.php
@@ -1,169 +1,302 @@
 <?
 
-
 require_once("include/bittorrent.php");
+
 dbconn(false);
 loggedinorreturn();
 
-$userid = intval($_GET['id']);
-$action = strval($_GET['action']);
+function friends_h($value) {
+	return htmlspecialchars_uni((string)$value);
+}
 
-if (!$userid)
-	$userid = $CURUSER['id'];
+function friends_user_menu($user) {
+	$id = (int)$user["id"];
+	$avatar = !empty($user["avatar"]) ? friends_h($user["avatar"]) : "/pic/default_avatar.gif";
+	$reputation = isset($user["simpaty"]) ? (int)$user["simpaty"] : 0;
+	$bonus = isset($user["bonus"]) ? number_format((float)$user["bonus"], 0, '.', ' ') : 0;
 
-if (!is_valid_id($userid))
+	return '
+	<div class="mn1_menu">
+		<ul class="men u2 w200">
+			<li class="img"><a href="/userdetails.php?id=' . $id . '"><img src="' . $avatar . '" class="p200" alt=""></a></li>
+			<li class="tp">Меню пользователя</li>
+			<li><span class="bulet"></span><a href="/inbox.php">Личные сообщения</a></li>
+			<li><span class="bulet"></span><a href="/userdetails.php?id=' . $id . '">Мой профиль</a></li>
+			<li><span class="bulet"></span><a href="/my.php">Редактировать профиль</a></li>
+			<li><span class="bulet"></span><a href="/mygroups.php">Мои группы</a></li>
+			<li><span class="bulet"></span><a href="/friends.php?id=' . $id . '">Мой список друзей</a></li>
+			<li class="sf"><span class="bulet"></span><a href="/mytorrents.php?id=' . $id . '">Мои раздачи</a></li>
+			<li class="tp">Репутация<span class="floatright"><a href="/pay_mode_b.php?userid=' . $id . '" title="Понизить репутацию"><img border="0" src="/pic/minus.gif" alt=""></a> <b>' . $reputation . '</b> <a href="/pay_mode_b.php?userid=' . $id . '" title="Повысить репутацию"><img border="0" src="/pic/plus.gif" alt=""></a></span></li>
+			<li><span class="bulet"></span><a href="/user_reputation.php?id=' . $id . '">Полученные отзывы</a></li>
+			<li><span class="bulet"></span><a href="/user_reputation.php?id=' . $id . '&amp;type=2">Отданные отзывы</a></li>
+			<li class="tp">Закладки</li>
+			<li><span class="bulet"></span><a href="/bookmarks.php?type=1">Раздачи</a></li>
+			<li><span class="bulet"></span><a href="/bookmarks.php?type=2">Группы</a></li>
+			<li><span class="bulet"></span><a href="/bookmarks.php?type=3">Пользователи</a></li>
+			<li class="sf"><span class="bulet"></span><a href="/bookmarks.php?type=4">Персоны</a></li>
+			<li class="tp">История</li>
+			<li><span class="bulet"></span><a href="/hytorrents.php?id=' . $id . '">Скачанного</a></li>
+			<li><span class="bulet"></span><a href="/userhistory.php?id=' . $id . '">Комментариев</a></li>
+			<li><span class="bulet"></span><a href="/uservotes.php?id=' . $id . '">Голосований</a></li>
+			<li class="tp">Голоса<span class="floatright b">' . $bonus . '</span></li>
+			<li><span class="bulet"></span><a href="/pay.php">Получить голоса</a></li>
+			<li><span class="bulet"></span><a href="/pay_mode.php">Управление голосами</a></li>
+			<li><span class="bulet"></span><a href="/pay_mode.php">Оставить пожелание</a></li>
+			<li><span class="bulet"></span><a href="/pay_mode.php">Обнулить счетчик скачиваний</a></li>
+		</ul>
+	</div>';
+}
+
+function friends_ago($timestamp) {
+	$timestamp = (string)$timestamp;
+	if ($timestamp === '' || $timestamp === '0000-00-00 00:00:00') {
+		return 'не был на сайте';
+	}
+
+	return get_et(sql_ts_to_ut($timestamp)) . ' назад';
+}
+
+function friends_delete_url($userid, $type, $targetid) {
+	global $CURUSER;
+
+	$hash = friends_h($CURUSER['hash4u'] ?? ($CURUSER['logout_hash'] ?? ''));
+	$url = '/friends.php?id=' . (int)$userid . '&amp;action=delete&amp;type=' . friends_h($type) . '&amp;targetid=' . (int)$targetid;
+	if ($hash !== '') {
+		$url .= '&amp;hash4u=' . $hash;
+	}
+
+	return $url;
+}
+
+function friends_render_user_card($row, $userid, $type) {
+	global $CURUSER;
+
+	$targetid = (int)$row['id'];
+	$username = (string)($row['username'] ?? '');
+	$userclass = (int)($row['class'] ?? 0);
+	$avatar = (($CURUSER["avatars"] ?? 'yes') === 'yes' && !empty($row["avatar"])) ? friends_h($row["avatar"]) : "/pic/default_avatar.gif";
+	$torrents = (int)($row['torrents_count'] ?? 0);
+	$comments = (int)($row['comments_count'] ?? 0);
+	$deleteText = $type === 'friend' ? 'Удалить' : 'Убрать';
+	$deleteUrl = friends_delete_url($userid, $type, $targetid);
+
+	if ($username === '') {
+		$username = 'Пользователь удален';
+	}
+
+	return "
+	<div class=\"pad5x0x0x5 mn2\">
+		<table class=\"tables2 w100p\">
+			<tr>
+				<td class=\"w50 top\"><img src=\"" . $avatar . "\" class=\"w50 rot180\" alt=\"\"></td>
+				<td class=\"top\">
+					<a href=\"/userdetails.php?id=" . $targetid . "\" class=\"u" . $userclass . "\">" . friends_h($username) . "</a>" . get_user_icons($row) . "<br>
+					<a href=\"/sendmessage.php?receiver=" . $targetid . "\" class=\"sba\">Сообщ.</a>
+					|
+					<a href=\"" . $deleteUrl . "\" class=\"sba\" onclick=\"return confirm('Удалить пользователя из списка?');\">" . $deleteText . "</a><br>
+					" . friends_ago($row['last_access'] ?? '') . "<br>
+					раздач <b>" . $torrents . "</b>, коммент. <b>" . $comments . "</b>
+				</td>
+			</tr>
+		</table>
+	</div>";
+}
+
+function friends_render_grid($rows, $userid, $type, $emptyText) {
+	if (!$rows) {
+		return '<div class="pad5x5">' . friends_h($emptyText) . '</div>';
+	}
+
+	$html = '<table class="tables1 w100p">';
+	$index = 0;
+
+	foreach ($rows as $row) {
+		if ($index % 2 === 0) {
+			$html .= '<tr>';
+		}
+
+		$html .= '<td class="w50p top">' . friends_render_user_card($row, $userid, $type) . '</td>';
+
+		if ($index % 2 === 1) {
+			$html .= '</tr>';
+		}
+
+		$index++;
+	}
+
+	if ($index % 2 === 1) {
+		$html .= '<td class="w50p top">&nbsp;</td></tr>';
+	}
+
+	return $html . '</table>';
+}
+
+$userid = (int)($_GET['id'] ?? $CURUSER['id']);
+$action = (string)($_GET['action'] ?? '');
+
+if (!$userid) {
+	$userid = (int)$CURUSER['id'];
+}
+
+if (!is_valid_id($userid)) {
 	stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
+}
 
-if ($userid != $CURUSER["id"])
+if ($userid !== (int)$CURUSER["id"]) {
 	stderr($tracker_lang['error'], $tracker_lang['access_denied']);
-
-$res = sql_query("SELECT * FROM users WHERE id=$userid") or sqlerr(__FILE__, __LINE__);
-$user = mysqli_fetch_array($res) or stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
-
-// action: add -------------------------------------------------------------
-
-if ($action == 'add')
-{
-	$targetid = intval($_GET['targetid']);
-	$type = strval($_GET['type']);
-
-  if (!is_valid_id($targetid))
-		stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
-
-  if ($type == 'friend') {
-  	$table_is = $frag = 'friends';
-    $field_is = 'friendid';
-  }	elseif ($type == 'block') {
-		$table_is = $frag = 'blocks';
-    $field_is = 'blockid';
-  } else
-		stderr($tracker_lang['error'], "Unknown type.");
-
-  $r = sql_query("SELECT id FROM $table_is WHERE userid=$userid AND $field_is=$targetid") or sqlerr(__FILE__, __LINE__);
-  if (mysqli_num_rows($r) == 1)
-		stderr($tracker_lang['error'], "User ID is already in your ".htmlentities($table_is)." list.");
-
-	sql_query("INSERT INTO $table_is VALUES (0,$userid, $targetid)") or sqlerr(__FILE__, __LINE__);
-  header("Location: $DEFAULTBASEURL/friends.php?id=$userid#$frag");
-  die;
 }
 
-// action: delete ----------------------------------------------------------
+$res = sql_query("SELECT * FROM users WHERE id = $userid LIMIT 1") or sqlerr(__FILE__, __LINE__);
+$user = mysqli_fetch_assoc($res) or stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
 
-if ($action == 'delete')
-{
-	$targetid = intval($_GET['targetid']);
-	$sure = htmlentities($_GET['sure']);
-	$type = htmlentities($_GET['type']);
+if ($action === 'add') {
+	$targetid = (int)($_GET['targetid'] ?? 0);
+	$type = (string)($_GET['type'] ?? '');
 
-  if (!is_valid_id($targetid))
+	if (!is_valid_id($targetid) || $targetid === $userid) {
 		stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
+	}
 
-  //if ($type == 'friend')
+	$target = sql_query("SELECT id FROM users WHERE id = $targetid LIMIT 1") or sqlerr(__FILE__, __LINE__);
+	if (mysqli_num_rows($target) !== 1) {
+		stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
+	}
 
-  if (!$sure)
-    stderr($tracker_lang['delete']." ".($type == 'friend'?$tracker_lang['friend']:$tracker_lang['block']),sprintf($tracker_lang['you_want_to_delete_x_click_here'],($type == 'friend'?$tracker_lang['friend']:$tracker_lang['block']),"?id=$userid&action=delete&type=$type&targetid=$targetid&sure=1"));
+	if ($type === 'friend') {
+		sql_query("DELETE FROM blocks WHERE userid = $userid AND blockid = $targetid") or sqlerr(__FILE__, __LINE__);
+		sql_query("INSERT IGNORE INTO friends (userid, friendid) VALUES ($userid, $targetid)") or sqlerr(__FILE__, __LINE__);
+		header("Location: $DEFAULTBASEURL/friends.php?id=$userid#friends");
+		exit;
+	}
 
-  if ($type == 'friend')
-  {
-    sql_query("DELETE FROM friends WHERE userid=$userid AND friendid=$targetid") or sqlerr(__FILE__, __LINE__);
-    if (mysql_affected_rows() == 0)
-      stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
-    $frag = "friends";
-  }
-  elseif ($type == 'block')
-  {
-    sql_query("DELETE FROM blocks WHERE userid=$userid AND blockid=$targetid") or sqlerr(__FILE__, __LINE__);
-    if (mysql_affected_rows() == 0)
-      stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
-    $frag = "blocks";
-  }
-  else
-    stderr($tracker_lang['error'], "Unknown type.");
+	if ($type === 'block') {
+		sql_query("DELETE FROM friends WHERE userid = $userid AND friendid = $targetid") or sqlerr(__FILE__, __LINE__);
+		sql_query("INSERT IGNORE INTO blocks (userid, blockid) VALUES ($userid, $targetid)") or sqlerr(__FILE__, __LINE__);
+		header("Location: $DEFAULTBASEURL/friends.php?id=$userid#blocks");
+		exit;
+	}
 
-  header("Location: $DEFAULTBASEURL/friends.php?id=$userid#$frag");
-  die;
+	stderr($tracker_lang['error'], 'Неизвестный тип списка.');
 }
 
-// main body  -----------------------------------------------------------------
+if ($action === 'delete') {
+	$targetid = (int)($_GET['targetid'] ?? 0);
+	$type = (string)($_GET['type'] ?? '');
+
+	if (!is_valid_id($targetid)) {
+		stderr($tracker_lang['error'], $tracker_lang['invalid_id']);
+	}
+
+	if ($type === 'friend') {
+		sql_query("DELETE FROM friends WHERE userid = $userid AND friendid = $targetid") or sqlerr(__FILE__, __LINE__);
+		header("Location: $DEFAULTBASEURL/friends.php?id=$userid#friends");
+		exit;
+	}
+
+	if ($type === 'block') {
+		sql_query("DELETE FROM blocks WHERE userid = $userid AND blockid = $targetid") or sqlerr(__FILE__, __LINE__);
+		header("Location: $DEFAULTBASEURL/friends.php?id=$userid#blocks");
+		exit;
+	}
+
+	stderr($tracker_lang['error'], 'Неизвестный тип списка.');
+}
+
+$friends = array();
+$res = sql_query("
+	SELECT
+		f.friendid AS id,
+		u.username,
+		u.class,
+		u.avatar,
+		u.title,
+		u.country,
+		u.gender,
+		u.donor,
+		u.warned,
+		u.enabled,
+		u.birthday,
+		u.last_access,
+		(SELECT COUNT(*) FROM torrents AS t WHERE t.owner = f.friendid) AS torrents_count,
+		(SELECT COUNT(*) FROM comments AS c WHERE c.user = f.friendid) AS comments_count
+	FROM friends AS f
+	LEFT JOIN users AS u ON f.friendid = u.id
+	WHERE f.userid = $userid
+	ORDER BY u.username ASC
+") or sqlerr(__FILE__, __LINE__);
+while ($row = mysqli_fetch_assoc($res)) {
+	$friends[] = $row;
+}
+
+$blocks = array();
+$res = sql_query("
+	SELECT
+		b.blockid AS id,
+		u.username,
+		u.class,
+		u.avatar,
+		u.title,
+		u.country,
+		u.gender,
+		u.donor,
+		u.warned,
+		u.enabled,
+		u.birthday,
+		u.last_access,
+		(SELECT COUNT(*) FROM torrents AS t WHERE t.owner = b.blockid) AS torrents_count,
+		(SELECT COUNT(*) FROM comments AS c WHERE c.user = b.blockid) AS comments_count
+	FROM blocks AS b
+	LEFT JOIN users AS u ON b.blockid = u.id
+	WHERE b.userid = $userid
+	ORDER BY u.username ASC
+") or sqlerr(__FILE__, __LINE__);
+while ($row = mysqli_fetch_assoc($res)) {
+	$blocks[] = $row;
+}
+
+$profile_class_css = 'u' . (int)($user["class"] ?? UC_USER);
+$hide_right_blocks = true;
 
 stdhead("Мои списки пользователей");
+?>
+<div class="mn_wrap">
+	<?= friends_user_menu($user) ?>
+	<div class="mn1_content">
+		<div class="bx1 <?= $profile_class_css ?>">
+			<a href="/userdetails.php?id=<?= $userid ?>" class="<?= $profile_class_css ?>"><?= friends_h($user['username']) ?></a>
+		</div>
 
-print("<table class=main width=100% border=0 cellspacing=0 cellpadding=0><tr><td class=embedded>");
+		<div class="bx1_0" id="friends">
+			<table class="tables1 w100p">
+				<tr>
+					<td class="<?= $profile_class_css ?>" colspan="2">
+						<span class="bulet"></span>
+						Список Ваших друзей
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<?= friends_render_grid($friends, $userid, 'friend', 'Ваш список друзей пуст.') ?>
+					</td>
+				</tr>
+			</table>
+		</div>
 
-print("<table width=100% border=1 cellspacing=0 cellpadding=5>");
-print("<tr><td class=\"colhead\"><a name=\"friends\">".$tracker_lang['friends_list']."</a></tr></td>");
-print("<tr><td>");
-$i = 0;
+		<div class="bx1" id="blocks">
+			<div class="<?= $profile_class_css ?>">
+				<span class="bulet"></span>
+				Список игнорированных
+			</div>
+			<?= friends_render_grid($blocks, $userid, 'block', 'Ваш список игнорированных пуст.') ?>
+		</div>
 
-$res = sql_query("SELECT f.friendid as id, u.username AS name, u.class, u.avatar, u.title, u.donor, u.warned, u.enabled, u.last_access FROM friends AS f LEFT JOIN users as u ON f.friendid = u.id WHERE userid=$userid ORDER BY name") or sqlerr(__FILE__, __LINE__);
-if(mysqli_num_rows($res) == 0)
-	$friends = "<em>".$tracker_lang['no_friends'].".</em>";
-else
-	while ($friend = mysqli_fetch_array($res))
-	{
-    $title = $friend["title"];
-		if (!$title)
-	    $title = get_user_class_name($friend["class"]);
-    $body1 = "<a href=\"userdetails.php?id=" . $friend['id'] . "\"><b>" . get_user_class_color($friend["class"], $friend['name']) . "</b></a>" .
-    	get_user_icons($friend) . " ($title)<br /><br />" . $tracker_lang['last_seen'] . $friend['last_access'] .
-    	"<br />(" . get_et(sql_ts_to_ut($friend[last_access])) . " ".$tracker_lang['ago'].")";
-		$body2 = "<br /><a href=\"friends.php?id=$userid&action=delete&type=friend&targetid=" . $friend['id'] . "\">".$tracker_lang['delete']."</a>" .
-			"<br /><br /><a href=\"sendmessage.php?receiver=" . $friend['id'] . "\">".$tracker_lang['pm']."</a>";
-    $avatar = ($CURUSER["avatars"] == "yes" ? htmlspecialchars_uni($friend["avatar"]) : "");
-		if (!$avatar)
-			$avatar = "pic/default_avatar.gif";
-    if ($i % 2 == 0)
-    	print("<table width=100% style='padding: 0px'><tr><td class=bottom style='padding: 5px' width=50% align=center>");
-    else
-    	print("<td class=bottom style='padding: 5px' width=50% align=center>");
-    print("<table class=main width=100% height=100px>");
-    print("<tr valign=top><td width=100 align=center style='padding: 0px'>" .
-			($avatar ? "<div style='width:100px;height:100px;overflow: hidden'><img width=\"100\" src=\"$avatar\" /></div>" : ""). "</td><td>\n");
-    print("<table class=main>");
-    print("<tr><td class=embedded style='padding: 5px' width=80%>$body1</td>\n");
-    print("<td class=embedded style='padding: 5px' width=20%>$body2</td></tr>\n");
-    print("</table>");
-		print("</td></tr>");
-		print("</td></tr></table>\n");
-    if ($i % 2 == 1)
-			print("</td></tr></table>\n");
-		else
-			print("</td>\n");
-		$i++;
-	}
-if ($i % 2 == 1)
-	print("<td class=bottom width=50%>&nbsp;</td></tr></table>\n");
-print($friends);
-print("</td></tr></table>\n");
-
-$res = sql_query("SELECT b.blockid AS id, u.username AS name, u.class, u.donor, u.warned, u.enabled, u.last_access FROM blocks AS b LEFT JOIN users AS u ON b.blockid = u.id WHERE userid = $userid ORDER BY name") or sqlerr(__FILE__, __LINE__);
-if(mysqli_num_rows($res) == 0)
-	$blocks = "<em>".$tracker_lang['no_blocked'].".</em>";
-else
-{
-	$i = 0;
-	$blocks = "<table width=100% cellspacing=0 cellpadding=0>";
-	while ($block = mysqli_fetch_array($res))
-	{
-		if ($i % 6 == 0)
-			$blocks .= "<tr>";
-    	$blocks .= "<td style='border: none; padding: 4px; spacing: 0px;'>[<font class=small><a href=friends.php?id=$userid&action=delete&type=block&targetid=" .
-				$block['id'] . ">D</a></font>] <a href=userdetails.php?id=" . $block['id'] . "><b>" . get_user_class_color($block['class'], $block['name']) . "</b></a>" .
-				get_user_icons($block) . "</td>";
-		if ($i % 6 == 5)
-			$blocks .= "</tr>";
-		$i++;
-	}
-	print("</table>\n");
-}
-print("<br />");
-print("<table class=main width=100% border=0 cellspacing=0 cellpadding=5>");
-print("<tr><td class=\"colhead\"><a name=\"blocks\">".$tracker_lang['blocked_list']."</a></td></tr>");
-print("<tr><td style='padding: 5px;background-color: #ECE9D8'>");
-print("$blocks\n");
-print("</td></tr></table>\n");
-print("</td></tr></table>\n");
-print("<p><a href=users.php><b>Найти пользователя/Список пользователей</b></a></p>");
+		<div class="bx1 center">
+			<a href="/users.php" class="sba">Найти пользователя / список пользователей</a>
+		</div>
+	</div>
+	<div class="clr"></div>
+</div>
+<?
 stdfoot();
+
 ?>
