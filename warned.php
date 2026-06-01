b7455ee03067df27d3fedb98d3b03510c2ccb816
nickmsk9 <v7kpdc272q@privaterelay.appleid.com>
2026-06-01 15:40:31 +0300

    правки

--- warned.php
+++ warned.php
@@ -1,102 +1,171 @@
 <?php
 
-/*
-// +--------------------------------------------------------------------------+
-// | Project:    TBDevYSE - TBDev Yuna Scatari Edition                        |
-// +--------------------------------------------------------------------------+
-// | This file is part of TBDevYSE. TBDevYSE is based on TBDev,               |
-// | originally by RedBeard of TorrentBits, extensively modified by           |
-// | Gartenzwerg.                                                             |
-// |                                                                          |
-// | TBDevYSE is free software; you can redistribute it and/or modify         |
-// | it under the terms of the GNU General Public License as published by     |
-// | the Free Software Foundation; either version 2 of the License, or        |
-// | (at your option) any later version.                                      |
-// |                                                                          |
-// | TBDevYSE is distributed in the hope that it will be useful,              |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with TBDevYSE; if not, write to the Free Software Foundation,      |
-// | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            |
-// +--------------------------------------------------------------------------+
-// |                                               Do not remove above lines! |
-// +--------------------------------------------------------------------------+
-*/
+declare(strict_types=1);
 
-require "include/bittorrent.php";
+require_once __DIR__ . '/include/bittorrent.php';
 
 dbconn();
-
 loggedinorreturn();
 
-if (get_user_class() < UC_MODERATOR)
-stderr($tracker_lang['error'], "Отказано в доступе.");
+if (get_user_class() < UC_MODERATOR) {
+    stderr($tracker_lang['error'] ?? 'Ошибка', 'Отказано в доступе.');
+}
 
-stdhead("Предупрежденные пользователи");
-$warned = number_format(get_row_count("users", "WHERE warned='yes'"));
-begin_frame("Предупрежденные пользователи: ($warned)", true);
-begin_table();
-
-$res = sql_query("SELECT * FROM users WHERE warned=1 AND enabled='yes' ORDER BY (users.uploaded/users.downloaded)") or sqlerr(__FILE__, __LINE__);
-$num = mysqli_num_rows($res);
-print("<table border=1 width=675 cellspacing=0 cellpadding=2><form action=\"nowarn.php\" method=post>\n");
-print("<tr align=center><td class=colhead width=90>Пользователь</td>
-<td class=colhead width=70>Зарегистрирован</td>
-<td class=colhead width=75>Последний&nbsp;раз&nbsp;был&nbsp;на&nbsp;трекере</td>
-<td class=colhead width=75>Класс</td>
-<td class=colhead width=70>Закачал</td>
-<td class=colhead width=70>Раздал</td>
-<td class=colhead width=45>Рейтинг</td>
-<td class=colhead width=125>Окончание</td>
-<td class=colhead width=65>Убрать</td>
-<td class=colhead width=65>Отключить</td></tr>\n");
-for ($i = 1; $i <= $num; $i++)
+function warned_h(mixed $value): string
 {
-$arr = mysql_fetch_assoc($res);
-if ($arr['added'] == '0000-00-00 00:00:00')
-$arr['added'] = '-';
-if ($arr['last_access'] == '0000-00-00 00:00:00')
-$arr['last_access'] = '-';
-
-
-if($arr["downloaded"] != 0){
-$ratio = number_format($arr["uploaded"] / $arr["downloaded"], 3);
-} else {
-$ratio="---";
+    return htmlspecialchars((string)$value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 }
-$ratio = "<font color=" . get_ratio_color($ratio) . ">$ratio</font>";
-$uploaded = mksize($arr["uploaded"]);
-$downloaded = mksize($arr["downloaded"]);
-// $uploaded = str_replace(" ", "<br />", mksize($arr["uploaded"]));
-// $downloaded = str_replace(" ", "<br />", mksize($arr["downloaded"]));
 
-$added = substr($arr['added'],0,10);
-$last_access = substr($arr['last_access'],0,10);
-$class=get_user_class_name($arr["class"]);
+function warned_date(?string $date): string
+{
+    if (
+        $date === null ||
+        $date === '' ||
+        $date === '0000-00-00 00:00:00' ||
+        $date === '0000-00-00'
+    ) {
+        return '-';
+    }
 
-print("<tr><td align=left><a href=userdetails.php?id=$arr[id]><b>$arr[username]</b></a>" .($arr["donor"] =="yes" ? "<img src=pic/star.gif border=0 alt='Donor'>" : "")."</td>
-<td align=center>$added</td>
-<td align=center>$last_access</td>
-<td align=center>$class</td>
-<td align=center>$downloaded</td>
-<td align=center>$uploaded</td>
-<td align=center>$ratio</td>
-<td align=center>$arr[warneduntil]</td>
-<td bgcolor=\"#008000\" align=center><input type=\"checkbox\" name=\"usernw[]\" value=\"$arr[id]\"></td>
-<td bgcolor=\"#FF000\" align=center><input type=\"checkbox\" name=\"desact[]\" value=\"$arr[id]\"></td></tr>\n");
+    return warned_h(substr($date, 0, 10));
 }
+
+function warned_ratio(float|int $uploaded, float|int $downloaded): string
+{
+    if ($downloaded <= 0) {
+        return '---';
+    }
+
+    $ratio = number_format($uploaded / $downloaded, 3, '.', '');
+    $color = get_ratio_color($ratio);
+
+    if ($color) {
+        return '<span style="color:' . warned_h($color) . '">' . warned_h($ratio) . '</span>';
+    }
+
+    return warned_h($ratio);
+}
+
+stdhead('Предупрежденные пользователи');
+
+$warnedCount = (int)get_row_count('users', "WHERE warned = 'yes'");
+
+begin_frame('Предупрежденные пользователи: (' . number_format($warnedCount) . ')', true);
+
+$res = sql_query("
+    SELECT
+        id,
+        username,
+        class,
+        donor,
+        added,
+        last_access,
+        uploaded,
+        downloaded,
+        warneduntil
+    FROM users
+    WHERE warned = 'yes'
+      AND enabled = 'yes'
+    ORDER BY
+        CASE
+            WHEN downloaded > 0 THEN uploaded / downloaded
+            ELSE uploaded
+        END ASC
+") or sqlerr(__FILE__, __LINE__);
+
+if (!$res instanceof mysqli_result) {
+    stderr($tracker_lang['error'] ?? 'Ошибка', 'Ошибка выполнения SQL-запроса.');
+}
+
+if (mysqli_num_rows($res) < 1) {
+    echo '<table class="tables1" width="100%" cellspacing="0" cellpadding="5">';
+    echo '<tr><td class="center">Предупрежденных пользователей нет.</td></tr>';
+    echo '</table>';
+
+    end_frame();
+    stdfoot();
+    exit;
+}
+
+echo '<form action="nowarn.php" method="post">';
+
+echo '<table class="tables1" width="100%" cellspacing="0" cellpadding="5">';
+
+echo '<tr class="colhead center">';
+echo '<td width="90">Пользователь</td>';
+echo '<td width="70">Зарегистрирован</td>';
+echo '<td width="75">Последний раз был на трекере</td>';
+echo '<td width="75">Класс</td>';
+echo '<td width="70">Закачал</td>';
+echo '<td width="70">Раздал</td>';
+echo '<td width="45">Рейтинг</td>';
+echo '<td width="125">Окончание</td>';
+echo '<td width="65">Убрать</td>';
+echo '<td width="65">Отключить</td>';
+echo '</tr>';
+
+while ($arr = mysqli_fetch_assoc($res)) {
+    $userId = (int)($arr['id'] ?? 0);
+
+    $username = warned_h($arr['username'] ?? '');
+    $userClass = get_user_class_name((int)($arr['class'] ?? 0));
+
+    $uploadedBytes = (float)($arr['uploaded'] ?? 0);
+    $downloadedBytes = (float)($arr['downloaded'] ?? 0);
+
+    $uploaded = warned_h(mksize($uploadedBytes));
+    $downloaded = warned_h(mksize($downloadedBytes));
+
+    $ratio = warned_ratio($uploadedBytes, $downloadedBytes);
+
+    $added = warned_date($arr['added'] ?? null);
+    $lastAccess = warned_date($arr['last_access'] ?? null);
+    $warnedUntil = warned_date($arr['warneduntil'] ?? null);
+
+    $donorIcon = '';
+    if (($arr['donor'] ?? '') === 'yes') {
+        $donorIcon = ' <img src="pic/star.gif" border="0" alt="Donor" title="Donor">';
+    }
+
+    echo '<tr>';
+
+    echo '<td align="left">';
+    echo '<a href="userdetails.php?id=' . $userId . '"><b>' . $username . '</b></a>' . $donorIcon;
+    echo '</td>';
+
+    echo '<td align="center">' . $added . '</td>';
+    echo '<td align="center">' . $lastAccess . '</td>';
+    echo '<td align="center">' . warned_h($userClass) . '</td>';
+    echo '<td align="center">' . $downloaded . '</td>';
+    echo '<td align="center">' . $uploaded . '</td>';
+    echo '<td align="center">' . $ratio . '</td>';
+    echo '<td align="center">' . $warnedUntil . '</td>';
+
+    echo '<td align="center" style="background:#008000">';
+    echo '<input type="checkbox" name="usernw[]" value="' . $userId . '">';
+    echo '</td>';
+
+    echo '<td align="center" style="background:#FF0000">';
+    echo '<input type="checkbox" name="desact[]" value="' . $userId . '">';
+    echo '</td>';
+
+    echo '</tr>';
+}
+
 if (get_user_class() >= UC_ADMINISTRATOR) {
-print("<tr><td colspan=10 align=right><input type=\"submit\" name=\"submit\" value=\"Применить\"></td></tr>\n");
-print("<input type=\"hidden\" name=\"nowarned\" value=\"nowarned\"></form></table>\n");
+    echo '<tr>';
+    echo '<td colspan="10" align="right">';
+    echo '<input type="hidden" name="nowarned" value="nowarned">';
+    echo '<input type="submit" name="submit" value="Применить">';
+    echo '</td>';
+    echo '</tr>';
 }
-print("<p>$pagemenu<br />$browsemenu</p>");
+
+echo '</table>';
+echo '</form>';
 
 end_frame();
 
-end_table();
-
 stdfoot();
+
 ?>
